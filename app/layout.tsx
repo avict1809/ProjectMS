372f89424dc9305dbe281da8c8a17091c8de769f
avict1809 <avict1809@proton.me>
2025-03-20 16:19:29 +0300

    Removed unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
