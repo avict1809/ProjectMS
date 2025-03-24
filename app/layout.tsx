abbf968217e85029a9ce94dd26dc2a6a11d1e874
avict1809 <avict1809@proton.me>
2025-03-24 16:03:36 +0300

    Mofified theming temporary

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Prevent Hydration Errors */}
         {mounted ? (
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+          <ThemeProvider attribute="class"  enableSystem={false}>
             {children}
           </ThemeProvider>
         ) : (
